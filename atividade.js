265477ad676a70fa7764b76e5a6de4f267e2e4eb
MARCUS VINÍCIUS FERREIRA LIMA <56344482800@info.local>
2026-09-15 11:01:10 -0300

    :sparkles: feat: Lista de exercicios passado em aula

---
+++ atividade.js
@@ -0,0 +1,180 @@
+//* Exercicios de repetição
+
+//* Nivel 1
+// for (i = 1; i <= 10; i++) {
+//     console.log(i)
+// }
+
+// let tabuada = prompt("Digite um Numero: ");
+
+// for(i = 1; i<=10; i++)
+// {
+//     console.log(tabuada * i);
+// }
+
+
+// let num3 = parseInt(prompt("Digite um Número para somar: "))
+// let soma = 0
+
+// for (i = 1; i <=num3; i++)
+// {
+//     soma = soma + i
+// }
+// console.log(soma)
+
+//* Nível Intermediario 
+
+// for(i = 0; i < 50; i++)
+// {
+//     if (i % 2 == 0)
+//     {
+//         console.log(i)
+//     }
+// }
+
+// let numAleatorio = 8
+// let numUsuario = prompt("Adivinhe o Numero: ")
+
+
+// while (numUsuario != numAleatorio)
+//     {
+//         let novoValor = prompt("Numero Errado digite Novamente: ")
+//         numUsuario = novoValor;
+        
+
+//     if (numUsuario == numAleatorio)
+//     {
+//         alert("Parábens! Numero correto!")
+//     }
+//     }
+
+
+//* Execicios de Condicional
+
+//* Nivel Basico
+// let idade = prompt("Digite sua idade: ");
+
+// if (idade < 18)
+// {
+//     console.log("Menor de Idade");
+// }
+
+// else 
+// {
+//     console.log("Maior de Idade");
+// }
+
+// let num = prompt("Digite um numero")
+
+// if (num < 0)
+// {
+//     console.log("Número Negativo")
+// }
+
+// else 
+// {
+//     console.log("Numero Positivo")
+// }
+
+// let nota = prompt("Digite sua Nota: ")
+
+// if (nota >= 60) 
+// {
+//     console.log("Aprovado")
+// }
+
+// else 
+// {
+//     console.log("Reprovado")
+// }
+
+// let num2 = prompt("Digite um Numero: ")
+
+// if (num2 > 0)
+// {
+//     console.log("Positivo")
+// }
+
+// else if (num2 == 0)
+// {
+//     console.log("Zero")
+// }
+
+// else 
+// {
+//     console.log("Negativo")
+// }
+
+// let classificacao = prompt("Digite sua Idade: ");
+
+// if (classificacao < 12)
+// {
+//     console.log("Criança")
+// }
+
+// else if (classificacao >=13 && classificacao <= 17)
+// {
+//     console.log("Adolescente")
+// }
+
+// else 
+// {
+//     console.log("Adulto")
+// }
+    
+
+// let numPar = prompt("Digite um Número: ")
+
+// if (numPar % 2 == 0)
+// {
+//     console.log(`${numPar} é Par!`)
+// }
+
+// else 
+// {
+//     console.log(`${numPar} é Impar!`)
+// }
+
+// let numero1 = parseInt(prompt("Digite um Numero: "))
+// let numero2 = parseInt(prompt("Digite outro Numero: "))
+// let operacao = prompt("Digite a operação desejada(+, -, /, *) ")
+
+// Primeiro if esta juntando os números não somando
+// if (operacao == "+")
+// {
+//     console.log(numero1 + numero2)
+// }
+
+// else if (operacao == "-")
+// {
+//     console.log(numero1 - numero2)
+// }
+
+// else if (operacao == "/")
+// {
+//     console.log(numero1 / numero2)
+// }
+
+// else if (operacao == "*")
+// {
+//     console.log(numero1 * numero2)
+//}
+
+// let numUser1 = prompt("Insira um número: ")
+// let numUser2 = prompt("Insira um segundo número: ")
+// let numUser3 = prompt("Insira um terceiro número: ")
+
+// if (numUser1 > numUser2 && numUser1 > numUser3)
+// {
+//     console.log(`${numUser1} é o maior numero!`)
+// }
+
+// if (numUser2 > numUser1 && numUser2 > numUser3)
+// {
+//     console.log(`${numUser2} é o maior numero!`)
+// }
+
+// if (numUser3 > numUser1 && numUser3 > numUser2)
+// {
+//     console.log(`${numUser3} é o maior numero!`)
+// }
--- script.js
+++ script.js
@@ -1,276 +1,276 @@
-// // FUNÇÔES NATIVAS JS
+// // // FUNÇÔES NATIVAS JS
 
-//*Função Map -> Percorre uma lista e cria uma nova lista com base na condição
-let numeros1 = [1,2,3,4];
+// //*Função Map -> Percorre uma lista e cria uma nova lista com base na condição
+// let numeros1 = [1,2,3,4];
 
-let numerosDobrados = numeros1.map(function(num) {
-    return num * 2;
-})
+// let numerosDobrados = numeros1.map(function(num) {
+//     return num * 2;
+// })
 
-console.log(numerosDobrados)
+// console.log(numerosDobrados)
 
-//Outro Modo -- Arrow Function com retorno implicito
-// let numerosDobradosArrow = numeros.map(num => num * 4);
+// //Outro Modo -- Arrow Function com retorno implicito
+// // let numerosDobradosArrow = numeros.map(num => num * 4);
 
-/* 
-    Quando usar Return ?
-    Quando a função precisa ter mais de uma linha
-    numeros.map(num => {
-     let resultado = num * 2;
-     return resultado;
-})
-*/ 
+// /* 
+//     Quando usar Return ?
+//     Quando a função precisa ter mais de uma linha
+//     numeros.map(num => {
+//      let resultado = num * 2;
+//      return resultado;
+// })
+// */ 
 
-//* FUNÇÂO FILTER
+// //* FUNÇÂO FILTER
 
-//? Filtra elementos com base em uma condição
-let numeros2 = [5, 10, 15, 20]
+// //? Filtra elementos com base em uma condição
+// let numeros2 = [5, 10, 15, 20]
 
-// num = palavra auxiliar
-let maioresQueDez = numeros2.filter(num => num > 10)
-console.log(maioresQueDez)
+// // num = palavra auxiliar
+// let maioresQueDez = numeros2.filter(num => num > 10)
+// console.log(maioresQueDez)
 
-// * FUNÇÃO REDUCE
-//? Reduz os valores de um Array para um único valor
-let numeros3 = [1,2,3,4];
+// // * FUNÇÃO REDUCE
+// //? Reduz os valores de um Array para um único valor
+// let numeros3 = [1,2,3,4];
 
-/// Acumulador Inicia em 0 e soma com o auxiliar até o último valor do array
-/// Variavel auxiliar é o num    
-let soma = numeros3.reduce((acumulador, auxiliar) => acumulador + auxiliar, 0);
-console.log(soma);
+// /// Acumulador Inicia em 0 e soma com o auxiliar até o último valor do array
+// /// Variavel auxiliar é o num    
+// let soma = numeros3.reduce((acumulador, auxiliar) => acumulador + auxiliar, 0);
+// console.log(soma);
 
-//* FUNÇÃO FIND
-//? Retorna o primeiro elemento que atende a condição 
+// //* FUNÇÃO FIND
+// //? Retorna o primeiro elemento que atende a condição 
 
-let produtos = [
-    {id: 1, nome: "teclado", preço: 50}, //Produto
-    {id: 2, nome: "mouse", preço: 30},
-    {id: 2, nome: "caderno", preço: 30},
-]
+// let produtos = [
+//     {id: 1, nome: "teclado", preço: 50}, //Produto
+//     {id: 2, nome: "mouse", preço: 30},
+//     {id: 2, nome: "caderno", preço: 30},
+// ]
  
-let item = produtos.find(produto => produto.id === 2);  // Find
-console.log(item)
+// let item = produtos.find(produto => produto.id === 2);  // Find
+// console.log(item)
 
-let itemFiltrado = produtos.filter(produto => produto.id === 2); // Filter
-console.log("Filter: " , itemFiltrado);
+// let itemFiltrado = produtos.filter(produto => produto.id === 2); // Filter
+// console.log("Filter: " , itemFiltrado);
 
-/* 
-    Diferença entre find e filter 
-    find -> retorna somente o primeiro elemento que atende a condição 
-    filter -> Retorna todos os elementos que atendam as condições 
-*/
+// /* 
+//     Diferença entre find e filter 
+//     find -> retorna somente o primeiro elemento que atende a condição 
+//     filter -> Retorna todos os elementos que atendam as condições 
+// */
 
-// * FUNÇÃO SPLIT
-//? Divide a string em partes transformando em um array
+// // * FUNÇÃO SPLIT
+// //? Divide a string em partes transformando em um array
 
-let frase = "Neymar no santos";
+// let frase = "Neymar no santos";
 
-// Com espaço separa as palavras pelo espaço
-let palavras = frase.split(" ");
-console.log(palavras);
+// // Com espaço separa as palavras pelo espaço
+// let palavras = frase.split(" ");
+// console.log(palavras);
 
-// Sem espaço , separa todas as letras dentro do array
-let palavras2 = frase.split("");
+// // Sem espaço , separa todas as letras dentro do array
+// let palavras2 = frase.split("");
 
-//* FUNÇÃO TRIM   
-//? trim remove espaços no inicio e no final de uma string
+// //* FUNÇÃO TRIM   
+// //? trim remove espaços no inicio e no final de uma string
 
-let nome = "    Neymar     ";
-let nomeLimpo = nome.trim();
+// let nome = "    Neymar     ";
+// let nomeLimpo = nome.trim();
 
-console.log(nome);
-console.log(nomeLimpo);
+// console.log(nome);
+// console.log(nomeLimpo);
 
-//* FUNÇÃO INCLUDES
-//? verifica se existe um valor dentro de uma lista e retorna um booleano (true ou false)
+// //* FUNÇÃO INCLUDES
+// //? verifica se existe um valor dentro de uma lista e retorna um booleano (true ou false)
 
-let frutas1 = ["maça", "banana"];
+// let frutas1 = ["maça", "banana"];
 
-let frutasInclude = frutas1.includes("banana");
-console.log(frutasInclude); true
+// let frutasInclude = frutas1.includes("banana");
+// console.log(frutasInclude); true
 
-//* FUNÇÃO 
-// toLowerCase -> transforma texto em minusculo
-// toUpperCase -> transforma texto em maiusculo
-let nome1 = "NEYMÁ";
-let cargo = "jogado";
+// //* FUNÇÃO 
+// // toLowerCase -> transforma texto em minusculo
+// // toUpperCase -> transforma texto em maiusculo
+// let nome1 = "NEYMÁ";
+// let cargo = "jogado";
 
-console.log(nome.toLowerCase());
-console.log(cargo.toUpperCase());
+// console.log(nome.toLowerCase());
+// console.log(cargo.toUpperCase());
 
-//* FUNÇÃO FOR EACH
-let nomes = ["neymar", "cebolinha", "gabigol", "coutinho"];
+// //* FUNÇÃO FOR EACH
+// let nomes = ["neymar", "cebolinha", "gabigol", "coutinho"];
 
-nomes.forEach(nome => console.log(`seu nome é: ${nome}`))
+// nomes.forEach(nome => console.log(`seu nome é: ${nome}`))
 
-//* FUNÇÃO SOME
-//? Verifica se pelo menos um item atende a condição - retorna apenas booleano
-let numeros4 = [1, 2, 3, 4];
+// //* FUNÇÃO SOME
+// //? Verifica se pelo menos um item atende a condição - retorna apenas booleano
+// let numeros4 = [1, 2, 3, 4];
 
-let temPar = numeros4.some(num => num % 2 === 0);
-console.log(temPar);
+// let temPar = numeros4.some(num => num % 2 === 0);
+// console.log(temPar);
 
-//* FUNÇÃO EVERY
-//? Verifica se todos os itens atenden a condição
+// //* FUNÇÃO EVERY
+// //? Verifica se todos os itens atenden a condição
 
-let todosPares = numeros4.every(num => num % 2 === 0);
-console.log(todosPares);
+// let todosPares = numeros4.every(num => num % 2 === 0);
+// console.log(todosPares);
 
-// * * FUNÇÃO SORT
-//? ordena os elementos do array
+// // * * FUNÇÃO SORT
+// //? ordena os elementos do array
 
-let numeros5 = [3, 10, 5, 2, 4]
-let letras = ["c", "d", "x", "a", "h"]
+// let numeros5 = [3, 10, 5, 2, 4]
+// let letras = ["c", "d", "x", "a", "h"]
 
-//Para letras 
-letras.sort();
-console.log(letras)
+// //Para letras 
+// letras.sort();
+// console.log(letras)
 
-//Para Números
-numeros5.sort((a,b) => a - b);
-console.log(numeros5);
+// //Para Números
+// numeros5.sort((a,b) => a - b);
+// console.log(numeros5);
 
-//* FUNÇÃO REVERSE
-let numeros6 = [1, 2, 3, 4]
+// //* FUNÇÃO REVERSE
+// let numeros6 = [1, 2, 3, 4]
 
-numeros6.reverse();
-console.log(numeros6)
+// numeros6.reverse();
+// console.log(numeros6)
 
-//* FUNÇÃO JOIN
-//? Junta os elementos de um array em uma string
-let palavras3 = ["Neymar", "no", "Santos"]
+// //* FUNÇÃO JOIN
+// //? Junta os elementos de um array em uma string
+// let palavras3 = ["Neymar", "no", "Santos"]
 
-let frase3 = palavras.join(" ");
-console.log(frase)
+// let frase3 = palavras.join(" ");
+// console.log(frase)
 
-//* FUNÇÕES PUSH, POP, SHIFT E UNSHIFT
-let lista = ["A", "B"];
+// //* FUNÇÕES PUSH, POP, SHIFT E UNSHIFT
+// let lista = ["A", "B"];
 
-//? push -> Adiciona elementos no final do array
-lista.push("C");
-console.log(lista)
+// //? push -> Adiciona elementos no final do array
+// lista.push("C");
+// console.log(lista)
 
-//? pop -> Remove elementos no final da lista
-lista.pop();
-console.log(lista)
+// //? pop -> Remove elementos no final da lista
+// lista.pop();
+// console.log(lista)
 
-//? shift -> Remove do inicio do array
-lista.shift();
-console.log(lista);
+// //? shift -> Remove do inicio do array
+// lista.shift();
+// console.log(lista);
 
-//? Unshift -> Adiciona elemento no inicio do array
-lista.unshift("A");
-console.log(lista);
+// //? Unshift -> Adiciona elemento no inicio do array
+// lista.unshift("A");
+// console.log(lista);
 
-//* FUNÇÃO SLICE
-//? Cria uma cópia de uma parte da lista
+// //* FUNÇÃO SLICE
+// //? Cria uma cópia de uma parte da lista
 
-/* 
-    array.slice(inicial, final)
+// /* 
+//     array.slice(inicial, final)
 
-    inicial -> onde começa
-    final -> onde termina porém não inclui (posição/indice/index) final
-*/
+//     inicial -> onde começa
+//     final -> onde termina porém não inclui (posição/indice/index) final
+// */
 
-let numeros7 = [1, 2, 3, 4]
+// let numeros7 = [1, 2, 3, 4]
 
-let parte = numeros7.slice(1,3) // 1 e 3 são as posições do array
-console.log(parte); // [2,3]
+// let parte = numeros7.slice(1,3) // 1 e 3 são as posições do array
+// console.log(parte); // [2,3]
 
-//* FUNÇÃO SPLICE
-//? remove ou adiociona elementos em QUALQUER POSIÇÃO 
+// //* FUNÇÃO SPLICE
+// //? remove ou adiociona elementos em QUALQUER POSIÇÃO 
 
-/* 
-    array.splice(index, count, item1, .... itemX);
+// /* 
+//     array.splice(index, count, item1, .... itemX);
 
-    index -> Índice (posição do elemento no array)
-    count -> Numero de itens a serem removidos (pode ser 0)
-    item1, ..., itemX -> Itens a serem adicionados
-*/
+//     index -> Índice (posição do elemento no array)
+//     count -> Numero de itens a serem removidos (pode ser 0)
+//     item1, ..., itemX -> Itens a serem adicionados
+// */
 
-let numeros8 = [1, 2, 3, 4];
+// let numeros8 = [1, 2, 3, 4];
 
-// Removendo elementos sem adicionar
-numeros8.splice(1, 1)
-console.log(numeros8);
+// // Removendo elementos sem adicionar
+// numeros8.splice(1, 1)
+// console.log(numeros8);
 
-let frutas = ["Banana", "Laranja", "Maçã", "Manga"];
+// let frutas = ["Banana", "Laranja", "Maçã", "Manga"];
 
-// Removendo elementos e adicionando mais 2
-// frutas.splice(0, 3, "Limão", "Kiwi");
+// // Removendo elementos e adicionando mais 2
+// // frutas.splice(0, 3, "Limão", "Kiwi");
+// // console.log(frutas);
+
+
+// frutas.splice(2, 0, "Limão", "Kiwi");
 // console.log(frutas);
 
+// //* FUNÇÃO REPLACE
+// //? Substitui uma parte da String
+// let texto = "Olá Mundo";
+// let novoTexto = texto.replace("Mundo", "Cliente");
+// console.log(novoTexto)
 
-frutas.splice(2, 0, "Limão", "Kiwi");
-console.log(frutas);
+// //* NOSSAS FUNÇÕES
 
-//* FUNÇÃO REPLACE
-//? Substitui uma parte da String
-let texto = "Olá Mundo";
-let novoTexto = texto.replace("Mundo", "Cliente");
-console.log(novoTexto)
+// function exibirMensagem() {
+//     console.log("Bem Vindo(a)!")
+// }
+// // executando a função criada
+// exibirMensagem();
 
-//* NOSSAS FUNÇÕES
+// // função com parametros
+// function cumprimentar(nome) {
+//     console.log(`Olá. ${nome}`)
+// }
 
-function exibirMensagem() {
-    console.log("Bem Vindo(a)!")
-}
-// executando a função criada
-exibirMensagem();
+// cumprimentar("Neymar");
+// cumprimentar("Gabigol");
 
-// função com parametros
-function cumprimentar(nome) {
-    console.log(`Olá. ${nome}`)
-}
+// // função com mais de um parâmetro
+// const somar = (numero9, numero10) => numero9 + numero10;
 
-cumprimentar("Neymar");
-cumprimentar("Gabigol");
+// console.log(somar(1, 2))
 
-// função com mais de um parâmetro
-const somar = (numero9, numero10) => numero9 + numero10;
+// function somarNovo(numero9, numero10) {
+//     console.log(numero9 + numero10);
 
-console.log(somar(1, 2))
+//     let resultado2 = numero9 + numero10;
+//     return resultado2;
+// }
 
-function somarNovo(numero9, numero10) {
-    console.log(numero9 + numero10);
+// //* FUNÇÃO PROMPT
+// //? Recebe/solicita informação do Usuário
 
-    let resultado2 = numero9 + numero10;
-    return resultado2;
-}
+// let nome3 = prompt("Digite seu Nome: ");
+// console.log(nome3);
+// alert(nome3);
 
-//* FUNÇÃO PROMPT
-//? Recebe/solicita informação do Usuário
+// // recebendo um número e convertendo
+// let idade = Number(prompt("Digite sua Idade: "));   
+// console.log(idade);
 
-let nome3 = prompt("Digite seu Nome: ");
-console.log(nome3);
-alert(nome3);
+// // parseInt() -> Converte valor para um número inteiro
+// idade = parseInt(prompt("Digite sua Idade: "));
+// console.log(idade);
+// // parseFloat() -> Converte valor para um número decimal/ponto flutuante
+// idade = parseFloat(prompt("Digite sua Idade: "));
+// console.log(idade);
 
-// recebendo um número e convertendo
-let idade = Number(prompt("Digite sua Idade: "));   
-console.log(idade);
+// // O que veremos nas proximas aulas
+// //Spoiler 
 
-// parseInt() -> Converte valor para um número inteiro
-idade = parseInt(prompt("Digite sua Idade: "));
-console.log(idade);
-// parseFloat() -> Converte valor para um número decimal/ponto flutuante
-idade = parseFloat(prompt("Digite sua Idade: "));
-console.log(idade);
+// let textoP = document.getElementById("texto");
 
-// O que veremos nas proximas aulas
-//Spoiler 
+// let saudacao = prompt("Digite a saudação: ");
 
-let textoP = document.getElementById("texto");
+// if (saudacao == "Bom dia") 
+//     {
+//         textoP.textContent = saudacao;
+//     }
 
-let saudacao = prompt("Digite a saudação: ");
-
-if (saudacao == "Bom dia") 
-    {
-        textoP.textContent = saudacao;
-    }
-
-else 
-    {
-        textoP.textContent = "SEGUNDOU!!!!!!!!"
-    }
+// else 
+//     {
+//         textoP.textContent = "SEGUNDOU!!!!!!!!"
+//     }
 
 
