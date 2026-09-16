d27064b2ee9c700e8f407abe58f66468bc4e22e2
MARCUS VINÍCIUS FERREIRA LIMA <56344482800@info.local>
2026-09-16 09:09:13 -0300

    :sparkles: feat: Adicionado outras funções de exemplos

--- manipulacao-dom/script.js
+++ manipulacao-dom/script.js
@@ -40,20 +40,90 @@ elementos.forEach(elemento => {
 */
 
 //Input
-const campo = document.getElementById("campo");
-const resultado = document.getElementById("resultado")
+// const campo = document.getElementById("campo");
+// const resultado = document.getElementById("resultado")
 
-// campo.addEventListener("input", () => {
+// // campo.addEventListener("input", () => {
+// //     // value -> o que está sendo digitado dentro do campo
+// //     resultado.innerText = campo.value;
+// // })
+
+
+// //Keyup
+// campo.addEventListener("keyup", () => {
 //     // value -> o que está sendo digitado dentro do campo
 //     resultado.innerText = campo.value;
 // })
 
+// EVENTO DE MOUSE
+// const elemento = document.getElementById("troca-cor");
+// const botao = document.getElementById("btn");
 
-//Keyup
-campo.addEventListener("keyup", () => {
-    // value -> o que está sendo digitado dentro do campo
-    resultado.innerText = campo.value;
+// //mouseover -> evento que dispara quando o mouse passar por cima
+// elemento.addEventListener("mouseover", () => {
+//     elemento.style.backgroundColor = "#FF0000";
+// })
+
+// //mouseout -> quando o mouse sai
+// elemento.addEventListener("mouseout", () => {
+//     elemento.style.backgroundColor = "#0000ffff"
+// }) 
+
+// mousemove
+// pegar a posição do mouse
+
+// document.addEventListener("mousemove", (evento) => {
+//     console.log(`Posição X: ${evento.clientX}, Posição Y: ${evento.clientY}`);
+// })
+
+// EVENTO DE FORMULÁRIO (submit)
+
+// const form = document.querySelector("form");
+
+// // Comportamento padrão do formulário é recarregar a pagina
+// form.addEventListener("submit", (e) => {
+//     //Impede o comportamento padrão do formulário
+//     e.preventDefault();
+
+//     // pego a informação digitada no input com o .value
+//     const nome = document.getElementById("nome").value;
+//     console.log(`Nome: ${nome}`);
+// })
+
+// Criando elementos na página
+
+// const novoElemento = document.createElement("p"); // Cria elemento <p> 
+// novoElemento.innerText = "Elemento novo criado"; // Cria um texto no elemento
+
+// const form = document.getElementById("form");
+
+// form.appendChild(novoElemento); // Estamos adicionando um elemento filho (p) dentro do pai (form);
+
+// const botao = document.createElement("button");
+// botao.innerText = "excluir elemento";
+// form.appendChild(botao);
+
+// botao.addEventListener("click", (e) => {
+//     e.preventDefault();
+
+//     novoElemento.remove();
+
+//     // Apagando direto do pai 
+//     //form.replaceChild(novoElemento)
+// })
+
+// Adicionando elementos em uma lista, a partir de um input
+const input = document.getElementById("input"); //input
+const botao = document.getElementById("add"); // button
+const lista = document.getElementById("lista"); // ul
+
+botao.addEventListener("click", () => {
+    const valorDigitado = input.value; // Pega o valor digitado da caixinha/input
+
+    const li = document.createElement("li");
+    li.innerText = valorDigitado;
+
+    lista.appendChild(li);
+
+    input.value = ""; 
 })
-
-
-
